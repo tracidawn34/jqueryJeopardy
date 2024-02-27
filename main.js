8af67d847cd739d7bdb8d1eecbf3f3710a938a14
justin beeler <tmcfall.prsvr@gmail.com>
2024-02-27 12:25:41 -0500

    html and css finished started on js

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+//const scoreAmount = $('div')
+//console.log('scoreAmount')
+
+//$('button').on('click', ()=>{
+   // const text = $('input')
+   // text.val('this changed') 
+//})
+
+const scoreDisplay = $('score-display')
+const questionDisplay = $('question')

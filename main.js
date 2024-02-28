4d283e2035e45abf076481a49c8ac60a5f0dfb81
justin beeler <tmcfall.prsvr@gmail.com>
2024-02-28 12:29:15 -0500

    fixed css and finishing js

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 
 
-//const scoreAmount = $('div')
-//console.log('scoreAmount')
+const scoreAmount = $('.grid-item')
+console.log('scoreAmount')
+
+const scoreDisplay = $('#score-display')
+const questionDisplay = $('.question')
+
+let score = 0
+
 
 //$('button').on('click', ()=>{
    // const text = $('input')
-   // text.val('this changed') 
+   // text.val('') 
 //})
 
-const scoreDisplay = $('score-display')
-const questionDisplay = $('question')
+
+scoreAmount.on('click', (event)=>{
+    console.log(event.target.innerText)
+     if(event.target.innerText === jeopardy.value){
+        $('.question').html(jeopardy.Math.random().question)
+    }
+})
+
+
+
+
+$('button').on('click', (event)=>{
+    //console.log('button pushed')
+    console.log(event.target.innerText)
+    const text = $('input')
+})

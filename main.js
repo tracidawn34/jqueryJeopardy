c7c21399e174eb4744442ab26158e909626ec36c
justin beeler <tmcfall.prsvr@gmail.com>
2024-03-04 07:20:29 -0500

    fixed css still fixing js questions

--- main.js
+++ main.js
@@ -26,14 +26,14 @@ scoreAmount.on('click', (event)=>{
     if(count === 0){
     value = event.target.innerHTML
     let arr = []
-    for(let obj of jeopardyQuestions){
-        if(obj.value === value){
-            arr.push(obj)
+    for(let id of jeopardyQuestions){
+        if(id.value === value){
+            arr.push(id)
         }
     }
     let findAnswer = Math.floor(Math.random()*arr.length)
-    let newQuestion = $(`<p>${arr[findAnswer-1].questionDisplay}</p>`)
-    correct = `${arr[findAnswer-1].answer}`
+    let newQuestion = $(`${arr[findAnswer].questionDisplay}`)
+    correct = `${arr[findAnswer].answer}`
     questionDisplay.append(newQuestion)
     console.log(correct)
     arr.length = 0
@@ -50,7 +50,7 @@ $('button').on('click', ()=>{
     //console.log(event.target.innerText)
     //const text = $('input')
     if(count > 0){
-        if($('#answer').val()=== correct){
+        if($('#answer').val() === correct){
             questionDisplay.text('correct')
             score += parseInt(value.split('$')[1])
             scoreDisplay.text(`${score}`)
